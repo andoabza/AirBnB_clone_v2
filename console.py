b42bd45409bffa22180875d6943984f949210322
anda <terbu102@gmail.com>
2023-09-17 23:07:24 -0700

    befoe

--- console.py
+++ console.py
@@ -78,7 +78,7 @@ class HBNBCommand(cmd.Cmd):
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
+                        #_args = _args.replace('\"', '')
             line = ' '.join([_cmd, _cls, _id, _args])
 
         except Exception as mess:
@@ -113,18 +113,26 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+    def do_create(self, arg):
+        """Create a new object"""
+        try:
+            if not args:
+                raise SyntaxError()
+                arg_list = args.split(" ")
+                kw = {}
+                for arg in arg_list[1:]:
+                    arg_splited = arg.split("=")
+                    arg_splited[1] = eval(arg_splited[1])
+                    if type(arg_splited[1]) is str:
+                        arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
+                    kw[arg_splited[0]] = arg_splited[1]
+        except SyntaxError:
+                print("** class name missing **")
+        except NameError:
+                print("** class doesn't exist **")
+        new_instance = HBNBCommand.classes[arg_list[0]](**kw)
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
