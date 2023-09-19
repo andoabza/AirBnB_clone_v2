516bbda2936ea998566011baf94b667584869f41
anda <terbu102@gmail.com>
2023-09-18 23:03:23 -0700

    relation ship

--- models/state.py
+++ models/state.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 class State(BaseModel, Base):
