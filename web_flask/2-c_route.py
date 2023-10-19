1d74e2ff170f7488505f45cac675b77b009093b0
anda <terbu102@gmail.com>
2023-10-19 04:41:05 -0700

    flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" print given param as arg"""
+from markupsafe import escape
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes=False
+@app.route("/")
+def hello():
+    """ print “Hello HBNB!”"""
+    return "Hello HBNB!"
+@app.route("/hbnb")
+def hbnb():
+    """ print in route hbnb"""
+    return "HBNB"
+@app.route('/c/<text>')
+def tex(text):
+    """ print text using escape"""
+    return f"C {escape(text)}"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
