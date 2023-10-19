e802b4a994ae4aeea0a6c46687ed681d98213ac3
anda <terbu102@gmail.com>
2023-10-19 00:02:04 -0700

    decorator

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-""" a flask script that print hell"""
+"""a script that print two things"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes=False
 @app.route("/")
 def hello():
-    """ a function tha return hello world"""
+    """ print “Hello HBNB!”"""
     return "Hello HBNB!"
-if __name__ == '__main__':
+@app.route("/hbnb")
+def hbnb():
+    """ print in route hbnb"""
+    return "HBNB"
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
